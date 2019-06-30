7b555ddd895b89c62a12b8353c48dd74b2ca5c3e
danmatlam <danmat_gd@hotmail.com>
2019-06-30 10:10:15 -0500

    agregar cors

--- API/server.js
+++ API/server.js
@@ -3,11 +3,15 @@ import path from "path";
 import express from "express";
 import bodyParser from "body-parser";
 import open from "open";
+import cors from "cors";
+
 import api from "./routes/api";
+
 
 const NODE_ENV = process.env.NODE_ENV || "DEVELOPMENT";
 const app = express();
 app.set("port", process.env.PORT || 3001);
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", api);

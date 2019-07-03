c21d75ae2dab90d54f3b334ae00994be4e1ab33c
danmatlam <danmat_gd@hotmail.com>
2019-07-03 11:57:24 -0500

    saveUsuarios

--- API/server.js
+++ API/server.js
@@ -6,16 +6,17 @@ import open from "open";
 import cors from "cors";
 import api from "./routes/api";
 const NODE_ENV = process.env.NODE_ENV || "DEVELOPMENT";
+const PORT = process.env.PORT || 3001;
 const app = express();
-app.set("port", process.env.PORT || 3001);
+app.set("port", PORT);
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", api);
 app.use("/", express.static(path.join(__dirname, "../src")));
 app.listen(app.get("port"), async (req, res) => {
-    console.log("Magic happens on port: ", NODE_ENV);
+    console.log("Magic happens on port: ", PORT);
     console.log("We are on: ", NODE_ENV);
-    // NODE_ENV === 'DEVELOPMENT' && await open("http://localhost:" + app.get("port") + "/api");
-    // await open("http://localhost:" + app.get("port"));
+    NODE_ENV === 'DEVELOPMENT' && await open("http://localhost:" + app.get("port") + "/api");
+    await open("http://localhost:" + app.get("port"));
 });
